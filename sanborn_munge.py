06518644332156ff164158fa4c1b1deb099a3099
outpw <philip.white@colorado.edu>
2022-05-21 12:02:40 -0600

    initial commit

---
+++ sanborn_munge.py
@@ -0,0 +1,33 @@
+import json
+import os
+import pprint
+import geopandas as gpd
+
+os.chdir(r'C:\Users\phwh9568\OneDrive - UCB-O365\UCB\Documents\Sanborn_Map')
+
+file = open('Staff_Map.json')
+
+inData = json.load(file)
+
+print(inData['features'][0])
+
+outPoints = dict(type='FeatureCollection',features=[])
+outLines = dict(type='FeatureCollection',features=[])
+#print(outData)
+
+for feature in inData['features']:
+    if feature['geometry']['type'] == 'Point':
+        outPoints['features'].append(feature)
+    if feature['geometry']['type'] == 'LineString':
+        outLines['features'].append(feature)
+
+
+#pprint.pprint(outData['features'])
+
+with open('Sanborn_Points.geojson', 'w') as outFile:
+    json.dump(outPoints,outFile, indent=4)
+with open('Sanborn_Lines.geojson', 'w') as outFile:
+    json.dump(outLines,outFile,indent=4)
+
+
+#outData['features'].append('x')
